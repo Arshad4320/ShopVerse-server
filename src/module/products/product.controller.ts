d03deb22bd7ca9b39fccabcda9a0e805c3b8d335
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-15 22:58:00 +0600

    product controller layer implemented

---
+++ src/module/products/product.controller.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from "express";
+import { ProductServices } from "./product.service";
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const payload = req.body;
+    const result = await ProductServices.createProduct(payload);
+    res.status(200).json({
+      success: true,
+      message: "product created successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getProducts();
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        message: "product is not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "data is retrived successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(404).json({
+        success: false,
+        message: "product id is not found",
+      });
+    }
+    const result = await ProductServices.getSingleProduct(productId);
+    res.status(200).json({
+      success: true,
+      message: "product is retrived successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(404).json({
+        success: false,
+        message: "product id is not found",
+      });
+    }
+    const payload = req.body;
+    const result = await ProductServices.updateProduct(productId, payload);
+    res.status(200).json({
+      success: true,
+      message: "product update successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(404).json({
+        success: false,
+        message: "product id is not found",
+      });
+    }
+    const result = await ProductServices.deleteProduct(productId);
+    res.status(200).json({
+      success: true,
+      message: "product deleted successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const ProductController = {
+  createProduct,
+  getProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+};
