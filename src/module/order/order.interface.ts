f390f0e0adff32072aa478661580fbdd5761289c
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-17 19:24:55 +0600

    order interface and order model implemented

---
+++ src/module/order/order.interface.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongoose";
+
+export interface IOrder {
+  user: ObjectId;
+  item: {
+    product: ObjectId;
+    quantity: number;
+    price: number;
+  }[];
+  address: {
+    name: string;
+    phone: string;
+    upozilla: string;
+    city: string;
+  };
+  paymentMethod: "COD" | "Bkash" | "Nagad" | "Card";
+  paymentStatus: "Pending" | "Cancel" | "Success" | "Failed";
+}
---
+++ src/module/order/order.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+import { IOrder } from "./order.interface";
+
+const OrderSchema = new Schema<IOrder>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  item: [
+    {
+      product: { type: Schema.Types.ObjectId, ref: "Product" },
+      quantity: {
+        type: Number,
+      },
+      price: {
+        type: Number,
+      },
+    },
+  ],
+  address: {
+    name: { type: String },
+    phone: { type: String },
+    upozilla: { type: String },
+    city: {
+      type: String,
+    },
+  },
+
+  paymentMethod: {
+    type: String,
+    enum: ["COD", "Bkash", "Nagad", "Card"],
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["Pending", "Cancel", "Success", "Failed"],
+  },
+});
+
+export const Order = model("Order", OrderSchema);
