b50b10aed43a8131c6ca2a106bfeb3ca90f198a8
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-26 23:04:11 +0600

    some change

--- src/module/user/user.routes.ts
+++ src/module/user/user.routes.ts
@@ -1,3 +1,48 @@
+// // src/modules/user/user.routes.ts
+
+// import express from "express";
+// import { UserController } from "./user.controller";
+// import { authorize } from "../../middleware/role.middleware";
+// import { verifyToken } from "../../middleware/verifyToken";
+
+// const router = express.Router();
+
+// router.post("/register-user", UserController.createUser);
+// router.post("/login-user", UserController.loginUser);
+
+// router.get("/self-user", verifyToken, UserController.getMyProfile);
+// router.patch("/self-user/update", verifyToken, UserController.updateMyProfile);
+
+// router.get(
+//   "/get-users",
+//   verifyToken,
+//   authorize("Admin"),
+//   UserController.getAllUser
+// );
+
+// router.get(
+//   "/get-user/:id",
+//   verifyToken,
+//   authorize("Admin"),
+//   UserController.getSingleUser
+// );
+
+// router.patch(
+//   "/update-user/:id",
+//   verifyToken,
+//   authorize("Admin"),
+//   UserController.updateUser
+// );
+
+// router.delete(
+//   "/delete-user/:id",
+//   verifyToken,
+//   authorize("Admin"),
+//   UserController.deleteUser
+// );
+
+// export const userRoute = router;
+
 // src/modules/user/user.routes.ts
 
 import express from "express";
@@ -13,30 +58,24 @@ router.post("/login-user", UserController.loginUser);
 router.get("/self-user", verifyToken, UserController.getMyProfile);
 router.patch("/self-user/update", verifyToken, UserController.updateMyProfile);
 
-router.get(
-  "/get-users",
-  verifyToken,
-  authorize("Admin"),
-  UserController.getAllUser
-);
+router.get("/get-users", UserController.getAllUser);
 
 router.get(
   "/get-user/:id",
-  verifyToken,
-  authorize("Admin"),
+
   UserController.getSingleUser
 );
 
 router.patch(
   "/update-user/:id",
-  verifyToken,
+
   authorize("Admin"),
   UserController.updateUser
 );
 
 router.delete(
   "/delete-user/:id",
-  verifyToken,
+
   authorize("Admin"),
   UserController.deleteUser
 );
