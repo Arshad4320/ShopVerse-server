92971d7429b72884cf8ec32f6a3e4f84c032b3b0
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-30 00:41:57 +0600

    some update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { router } from "./router/router";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
     success: true,
