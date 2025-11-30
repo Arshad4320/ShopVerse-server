0028c69820ee5e77d349d14d033f45d54121aa80
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-30 19:50:03 +0600

    some update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-// import app from "./app";
-// import config from "./config/index";
-// import connectDB from "./config/db.config";
-// // import serverless from "serverless-http";
-// const startServer = async () => {
-//   await connectDB();
-//   app.listen(config.port, () => {
-//     console.log("shop verse Server is Running On Port", config.port);
-//   });
-// };
-
-// startServer();
-// export default serverless(app);
 import app from "./app";
+import config from "./config/index";
 import connectDB from "./config/db.config";
-import serverless from "serverless-http";
+// import serverless from "serverless-http";
+const startServer = async () => {
+  await connectDB();
+  app.listen(config.port, () => {
+    console.log("shop verse Server is Running On Port", config.port);
+  });
+};
 
-connectDB(); // Only call DB connect once
-
-export const handler = serverless(app);
-export default handler;
+startServer();
+// export default serverless(app);
