09f7942969ebf03ccf45463227a8020649ab0ed5
Arshad Ullah <mdarshadullah835@gmail.com>
2025-11-30 18:58:29 +0600

    some issue solved

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+// import app from "./app";
+// import config from "./config/index";
+// import connectDB from "./config/db.config";
+// import serverless from "serverless-http";
+// const startServer = async () => {
+//   await connectDB();
+//   app.listen(config.port, () => {
+//     console.log("shop verse Server is Running On Port", config.port);
+//   });
+// };
+
+// startServer();
+// export default serverless(app);
 import app from "./app";
-import config from "./config/index";
 import connectDB from "./config/db.config";
 import serverless from "serverless-http";
-const startServer = async () => {
-  await connectDB();
-  app.listen(config.port, () => {
-    console.log("shop verse Server is Running On Port", config.port);
-  });
-};
 
-startServer();
-export default serverless(app);
+connectDB(); // only call this once
+
+export const handler = serverless(app);
+export default handler;
